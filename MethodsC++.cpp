f1170b1267145066f46fbd5a39cea7af78eaaa62
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-06-10 10:33:47 +0300

    Puttern

--- MethodsC++.cpp
+++ MethodsC++.cpp
@@ -1,9 +1,47 @@
-// MethodsC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+class Target
 {
-    std::cout << "Hello World!\n";
+public:
+    virtual void Request() = 0;
+};
+
+class Adaptee
+{
+public:
+    void SpecificRequest()
+    {
+        cout << "Adaptee.SpecificRequest()" << endl;
+    }
+};
+
+class Adapter :public Target, public Adaptee
+{
+public:
+    void Request()
+    {
+        SpecificRequest();
+    }
+};
+
+class Client
+{
+public:
+    void ClientMethod(Target& target)
+    {
+        target.Request();
+    }
+
+    void ClientMethod(Target* target)
+    {
+        target->Request();
+    }
+};
+
+void main()
+{
+    Client client;
+    Target* target = (Target*) new Adapter();
+    client.ClientMethod(target); 
 }
